8d313290f7c01a5c5e10a92ceaba9814aacf9e19
J.Javier Reig <jreigpas@nttdata.com>
2022-04-20 15:41:33 +0200

    adding routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css';
-import CmpDeClase from './components/CmpDeClase';
-import CmpFuncion from './components/CmpFuncion';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import CmpDeClase from "./components/CmpDeClase";
+import CmpFuncion from "./components/CmpFuncion";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-       <CmpFuncion />
-       <CmpDeClase />
-    </div>
+    <BrowserRouter>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/funcion">funcion</Link> |{" "}
+        <Link to="/clase">clase</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/funcion" element={<CmpFuncion />} />
+        <Route path="/clase" element={<CmpDeClase />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  const nombre = "Juan";
+  const getNombre = () => nombre;
+
+  return (
+    <div>
+      <h1>Esta es la home</h1>
+    </div>
+  );
+};
+
+export default Home;
